0ee18b4ae3f7a1b3988d014fa12e15a13eb73047
shimmy77 <christianshim@gmail.com>
2020-01-07 13:42:36 -0600

    created iterating properties

---
+++ Iterating_Properties.js
@@ -0,0 +1,12 @@
+const obj = {
+    foo: 'hi',
+    bar: 'bye',
+    fum: 'smoke',
+    quux: 'unknown',
+    spam: 'spam',
+};
+
+for (const key in obj) {
+    obj[key]
+    console.log(obj[key]);
+}
