9a44737337052dda15161df2d983004b7c5dbc94
shimmy77 <christianshim@gmail.com>
2020-01-07 16:37:44 -0600

    Completed enroll summer school

--- LOTR.js
+++ LOTR.js
@@ -25,18 +25,6 @@ function createCharacter(name, nickname, race, origin, attack, defense, weapon) 
     }
 }
 
-<<<<<<< HEAD
-const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
-                    createCharacter('Bilbo Baggins', 'biblo', 'Hobbit', 'The Shire', 2, 1),
-                    createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
-                    createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
-                    createCharacter('Legolas', 'legolas', 'Elf', 'Wooland realm', 8, 5),
-                    createCharacter('Arwen Undomiel', 'elfie', 'Half-elf', 'Rivendell', 12, 22),
-                    ];
-
-//console.log(characters);
-const aragorn = characters.find(person => person['nickname'] === 'aragorn').describe();
-=======
 const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'Staff'),
                     createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'Ring'),
                     createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2, 'String and Barrow Blade'),
@@ -52,7 +40,6 @@ console.log(hobbits);
 const strongAttack = characters.filter(character => character['attack'] > 5);
 console.log(strongAttack);
 
->>>>>>> 2d8d4e2a4e3445412a4e3792b70eda6aa376314e
 
 // const frodo = createCharacter('Frodo', 'froyo', 'hobbit', 'the shire', 220, 10);
 // const gimli = createCharacter('gimli', 'dwarf', 'dwarf', 'the mountain', 22, 2220);
