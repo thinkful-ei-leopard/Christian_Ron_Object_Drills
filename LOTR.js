b46d661345485c09c2744e63c6f2f6de041bdbbb
Ron Martin <ronmartin89@gmail.com>
2020-01-07 13:22:48 -0800

    bashing my head on the wall

--- LOTR.js
+++ LOTR.js
@@ -23,61 +23,65 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
     }
 }
 
-const characters = [
-    {
-        name: 'Gandalf the White',
-        nickname: 'gandalf',
-        race: 'Wizard',
-        origin: 'Middle Earth',
-        attack: 10,
-        defense: 6
-    },
-    {
-        name: 'Bilbo Baggins',
-        nickname: 'bilbo',
-        race: 'Hobbit',
-        origin: 'The Shire',
-        attack: 2,
-        defense: 1
-    },
-    {
-        name: 'Frodo Baggins',
-        nickname: 'frodo',
-        race: 'Hobbit',
-        origin: 'The Shire',
-        attack: 3
-        defense: 2
-    },
-    {
-        name: 'Aragorn son of Arathorn',
-        nickname: 'aragorn',
-        race: 'Man',
-        origin: 'Dunnedain',
-        attack: 6
-        defense: 8
-    },
-    {
-        name: 'Legolas',
-        nickname: 'legolas',
-        race: 'Elf',
-        origin: 'Woodland Realm',
-        attack: 8,
-        defense: 5
-    },
-    {
-        name: 'Arwen Undomiel',
-        nickname: 'elfie',
-        race: 'Half-Elf',
-        origin: 'Rivendell',
-        attack: 12,
-        defense: 22
-    },
-];
+const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6)];
+// const characters = [
+//     {
+//         name: 'Gandalf the White',
+//         nickname: 'gandalf',
+//         race: 'Wizard',
+//         origin: 'Middle Earth',
+//         attack: 10,
+//         defense: 6
+//     },
+//     {
+//         name: 'Bilbo Baggins',
+//         nickname: 'bilbo',
+//         race: 'Hobbit',
+//         origin: 'The Shire',
+//         attack: 2,
+//         defense: 1
+//     },
+//     {
+//         name: 'Frodo Baggins',
+//         nickname: 'frodo',
+//         race: 'Hobbit',
+//         origin: 'The Shire',
+//         attack: 3,
+//         defense: 2
+//     },
+//     {
+//         name: 'Aragorn son of Arathorn',
+//         nickname: 'aragorn',
+//         race: 'Man',
+//         origin: 'Dunnedain',
+//         attack: 6,
+//         defense: 8
+//     },
+//     {
+//         name: 'Legolas',
+//         nickname: 'legolas',
+//         race: 'Elf',
+//         origin: 'Woodland Realm',
+//         attack: 8,
+//         defense: 5
+//     },
+//     {
+//         name: 'Arwen Undomiel',
+//         nickname: 'elfie',
+//         race: 'Half-Elf',
+//         origin: 'Rivendell',
+//         attack: 12,
+//         defense: 22
+//     },
+// ];
 
-character.forEach(char => createCharacter(char));
+// characters.forEach(item => {
+//     characters[item] = createCharacter(item);
+// });
+console.log(characters);
 
-const frodo = createCharacter('Frodo', 'froyo', 'hobbit', 'the shire', 220, 10);
-const gimli = createCharacter('gimli', 'dwarf', 'dwarf', 'the mountain', 22, 2220);
+// const frodo = createCharacter('Frodo', 'froyo', 'hobbit', 'the shire', 220, 10);
+// const gimli = createCharacter('gimli', 'dwarf', 'dwarf', 'the mountain', 22, 2220);
 
-const whosWins = frodo.evaluateFight(gimli);
-console.log(whosWins);
+// const whosWins = frodo.evaluateFight(gimli);
+// console.log(whosWins);
