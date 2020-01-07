ef4434746d06818a232c6cacaefb47885f779a97
shimmy77 <christianshim@gmail.com>
2020-01-07 13:50:34 -0600

    created array of obj

---
+++ Arrays_of_Object.js
@@ -0,0 +1,7 @@
+const array = [
+    { name: 'Ron', jobTitle: 'Fullstack Developer'},
+    { name: 'Christian', jobTitle: 'Fullstack Developer'},
+    { name: 'Frodo', jobTitle: 'Hobbit'}
+];
+
+array.forEach(obj => console.log(obj.name, obj.jobTitle));
