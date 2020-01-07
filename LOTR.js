2d8d4e2a4e3445412a4e3792b70eda6aa376314e
shimmy77 <christianshim@gmail.com>
2020-01-07 16:09:36 -0600

    edited LOTR

--- LOTR.js
+++ LOTR.js
@@ -1,4 +1,5 @@
-function createCharacter(name, nickname, race, origin, attack, defense) {
+
+function createCharacter(name, nickname, race, origin, attack, defense, weapon) {
     return {
         name,
         nickname,
@@ -6,8 +7,9 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
         origin,
         attack,
         defense,
+        weapon,
         describe() {
-            console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+            console.log(`${this.name} is a ${this.race} from ${this.origin} who uses ${this.weapon}.`);
         },
         evaluateFight(character) {
             let result1 = 0;
@@ -23,15 +25,21 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
     }
 }
 
-const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
-                    createCharacter('Bilbo Baggins', 'biblo', 'Hobbit', 'The Shire', 2, 1),
-                    createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
-                    createCharacter('Aragon son of Arathorn', 'aragon', 'Man', 'Dunnedain', 6, 8),
-                    createCharacter('Legolas', 'legolas', 'Elf', 'Wooland realm', 8, 5),
-                    createCharacter('Arwen Undomiel', 'elfie', 'Half-elf', 'Rivendell', 12, 22),
+const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'Staff'),
+                    createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'Ring'),
+                    createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2, 'String and Barrow Blade'),
+                    createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8, 'Anduril'),
+                    createCharacter('Legolas', 'legolas', 'Elf', 'Wooland realm', 8, 5, 'Bow'),
+                    createCharacter('Arwen Undomiel', 'elfie', 'Half-elf', 'Rivendell', 12, 22, 'Hadhafang')
                     ];
 
-console.log(characters);
+console.log(characters);                    
+const legolas = characters.find(person => {return person['nickname'] === 'legolas'}).describe();
+const hobbits = characters.filter(character => character['race'] === 'Hobbit');
+console.log(hobbits);
+const strongAttack = characters.filter(character => character['attack'] > 5);
+console.log(strongAttack);
+
 
 // const frodo = createCharacter('Frodo', 'froyo', 'hobbit', 'the shire', 220, 10);
 // const gimli = createCharacter('gimli', 'dwarf', 'dwarf', 'the mountain', 22, 2220);
